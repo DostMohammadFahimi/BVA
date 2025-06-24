40e374c8e4fc4fee5591ed7d5fea03e1b7efe820
Dost Mohammad Fahimi <dmfahimiiiii730@gmail.com>
2025-06-24 10:24:01 +0430

    create the token for login  and also work on the logout that clrear the tokens and aslo add the user model .

---
+++ controllers/auth.controller.js
@@ -0,0 +1,74 @@
+import bcrypt from "bcryptjs";
+import User from "../modules/user.model.js";
+import genToken from "../config/token.js";
+
+export const singUp = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existEmail = await User.findOne({ email });
+    if (existEmail) {
+      return res.status(400).json({ message: "Email alredy exsit!" });
+    }
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters!",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      password: hashedPassword,
+      email,
+    });
+
+    const token = genToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      samesite: "strict",
+      secure: false,
+    });
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `sing up error ${error}` });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Email does not exsit!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    const token = genToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      samesite: "strict",
+      secure: false,
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `login up error ${error}` });
+  }
+};
+
+export const logOut = async (req, res) => {
+  try {
+    res.clearToken("token");
+    return res.status(200).json({
+      message: "logout sucssesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: `logout up error ${error}` });
+  }
+};
